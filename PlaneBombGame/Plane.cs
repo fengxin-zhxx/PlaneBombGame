6d30e7c2a048ae7a77de300199cf11f7f8c72a26
FUFU <2675007059@qq.com>
2022-11-01 20:51:08 +0800

    Update Plane.cs

--- PlaneBombGame/Plane.cs
+++ PlaneBombGame/Plane.cs
@@ -24,8 +24,7 @@ namespace PlaneBombGame
         }
 
         //加入飞机方向判断
-        public void Draw(Panel p)
-        {
+        public void Draw(Panel p) {
             Graphics g = p.CreateGraphics();                // 创建面板画布
 
             g.SmoothingMode = SmoothingMode.HighQuality;
@@ -39,36 +38,78 @@ namespace PlaneBombGame
             int dx = StandardSize.BlockWidth;
             int dy = dx;
             Rectangle[] recs = new Rectangle[12];
+            // Ellipse[] ells = new Ellipse[4];
             // 0
-            recs[0] = new Rectangle(AccurateX, AccurateY - 2 * dy, dx, 5 * dy);  //长横
-            recs[1] = new Rectangle(AccurateX - 2 * dx, AccurateY - dy, dx, 3 * dy); //短横
-            recs[2] = new Rectangle(AccurateX - 2 * dx, AccurateY, 4 * dx, dy); //一竖
+            recs[0] = new Rectangle(AccurateX, AccurateY - dy, dx, 3 * dy);  //长横
+            recs[1] = new Rectangle(AccurateX - 2 * dx, AccurateY, dx, dy); //短横
+            recs[2] = new Rectangle(AccurateX - 2 * dx, AccurateY, 3 * dx, dy); //一竖
             // 1
-            recs[3] = new Rectangle(AccurateX - 2 * dx, AccurateY, 5 * dx, dy);
-            recs[4] = new Rectangle(AccurateX - dx, AccurateY - 2 * dy, 3 * dx, dy);
-            recs[5] = new Rectangle(AccurateX, AccurateY - 2 * dy, dx, 4 * dy);
+            recs[3] = new Rectangle(AccurateX - dx, AccurateY, 3 * dx, dy);
+            recs[4] = new Rectangle(AccurateX, AccurateY - 2 * dy, dx, dy);
+            recs[5] = new Rectangle(AccurateX, AccurateY - 2 * dy, dx, 3 * dy);
             // 2 
-            recs[6] = new Rectangle(AccurateX, AccurateY - 2 * dy, dx, 5 * dy);
-            recs[7] = new Rectangle(AccurateX + 2 * dx, AccurateY - dy, dx, 3 * dy);
-            recs[8] = new Rectangle(AccurateX - dx, AccurateY, 4 * dx, dy);
+            recs[6] = new Rectangle(AccurateX, AccurateY - dy, dx, 3 * dy);
+            recs[7] = new Rectangle(AccurateX + 2 * dx, AccurateY, dx, dy);
+            recs[8] = new Rectangle(AccurateX, AccurateY, 3 * dx, dy);
             // 3
-            recs[9] = new Rectangle(AccurateX - 2 * dx, AccurateY, 5 * dx, dy);
-            recs[10] = new Rectangle(AccurateX - dx, AccurateY + 2 * dy, 3 * dx, dy);
-            recs[11] = new Rectangle(AccurateX, AccurateY - dy, dx, 4 * dy);
+            recs[9] = new Rectangle(AccurateX - dx, AccurateY, 3 * dx, dy);
+            recs[10] = new Rectangle(AccurateX, AccurateY + 2 * dy, dx, dy);
+            recs[11] = new Rectangle(AccurateX, AccurateY, dx, 3 * dy);
 
 
-
-            Pen pen = new Pen(Color.Black);
-            for (int i = 0; i < 3; i++)
-            {
+            //direction = 3;
+            /*Pen pen = new Pen(Color.Black);
+            for (int i = 0; i < 3; i++) {
                 g.DrawRectangle(pen, recs[direction * 3 + i]);
-            }
-            //LinearGradientBrush brush=new LinearGradientBrush()
-            SolidBrush brush = new SolidBrush(Color.Blue);
-            for (int i = 0; i < 3; i++)
-            {
+            }*/
+            SolidBrush brush = new SolidBrush(Color.SteelBlue);
+            for (int i = 0; i < 3; i++) {
                 g.FillRectangle(brush, recs[direction * 3 + i]);
             }
+
+            Rectangle[] rc = new Rectangle[20];
+
+            //头
+            rc[0] = new Rectangle(AccurateX, AccurateY, 2 * dx, dy);
+            rc[5] = new Rectangle(AccurateX, AccurateY, dx, 2 * dy);
+            rc[10] = new Rectangle(AccurateX - dx, AccurateY, 2 * dx, dy);
+            rc[15] = new Rectangle(AccurateX, AccurateY - dy, dx, 2 * dy);
+
+            //0
+            rc[1] = new Rectangle(AccurateX, AccurateY - 2 * dy, dx, 2 * dy);
+            rc[2] = new Rectangle(AccurateX, AccurateY + dy, dx, 2 * dy);
+            rc[3] = new Rectangle(AccurateX - 2 * dx, AccurateY - dy, dx, 2 * dy);
+            rc[4] = new Rectangle(AccurateX - 2 * dx, AccurateY, dx, 2 * dy);
+            //1
+            rc[6] = new Rectangle(AccurateX + dx, AccurateY, 2 * dx, dy);
+            rc[7] = new Rectangle(AccurateX - 2 * dx, AccurateY, 2 * dx, dy);
+            rc[8] = new Rectangle(AccurateX - dx, AccurateY - 2 * dy, 2 * dx, dy);
+            rc[9] = new Rectangle(AccurateX, AccurateY - 2 * dy, 2 * dx, dy);
+            //2
+            rc[11] = rc[1];
+            rc[12] = rc[2];
+            rc[13] = new Rectangle(AccurateX + 2 * dx, AccurateY - dy, dx, 2 * dy);
+            rc[14] = new Rectangle(AccurateX + 2 * dx, AccurateY, dx, 2 * dy);
+            //3
+            rc[16] = rc[6];
+            rc[17] = rc[7];
+            rc[18] = new Rectangle(AccurateX - dx, AccurateY + 2 * dy, 2 * dx, dy);
+            rc[19] = new Rectangle(AccurateX, AccurateY + 2 * dy, 2 * dx, dy);
+
+
+
+
+            SolidBrush brush2 = new SolidBrush(Color.Yellow);
+
+            for (int i = 0; i < 5; i++) {
+                g.FillEllipse(brush, rc[5 * direction + i]);
+            }
+
+            //Bitmap bt = new Bitmap("D:\\桌面\\大学\\大三上\\C#\\蓝色飞机.png");
+            //TextureBrush tb = new TextureBrush(bt);
+            //for(int i = 0; i < 3; i++) {
+            //    g.FillRectangle(tb, recs[direction * 3 + i]);
+            //}
 
             //g.FillEllipse(new SolidBrush(Color.Blue), AccurateX, AccurateY, StandardSize.BlockWidth, StandardSize.BlockWidth);
         }
