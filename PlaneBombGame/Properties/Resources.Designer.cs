5c78f957250153004d761b8f81ddd2cd9588ed5f
FengXin <1638547819@qq.com>
2022-11-04 11:26:12 +0800

    mergeUI

--- PlaneBombGame/Properties/Resources.Designer.cs
+++ PlaneBombGame/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace PlaneBombGame.Properties {
     // 类通过类似于 ResGen 或 Visual Studio 的工具自动生成的。
     // 若要添加或移除成员，请编辑 .ResX 文件，然后重新运行 ResGen
     // (以 /str 作为命令选项)，或重新生成 VS 项目。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -87,9 +87,7 @@ namespace PlaneBombGame.Properties {
         ///7 5 0 2 8 2 2 3 2
         ///7 5 1 2 8 2 2 3 2
         ///7 5 2 2 8 2 2 3 2
-        ///7 5 3 2 8 2 2 3 2
-        ///7 6 0 2 8 2 2 3 2
-        ///7 6 1 2  [字符串的其余部分被截断]&quot;; 的本地化字符串。
+        ///7 5 3 2 8 2 2 3 2        /// [字符串的其余部分被截断]&quot;; 的本地化字符串。
         /// </summary>
         internal static string All {
             get {
