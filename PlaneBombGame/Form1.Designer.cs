266fdc1f93fdc8b23d8c8a1062f21064bf3e4126
FengXin <1638547819@qq.com>
2022-11-08 00:06:34 +0800

    添加icon

--- PlaneBombGame/Form1.Designer.cs
+++ PlaneBombGame/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel2 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -54,27 +55,18 @@
             this.panel2.Controls.Add(this.button1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1484, 60);
+            this.panel2.Size = new System.Drawing.Size(1979, 75);
             this.panel2.TabIndex = 0;
-            // label4
-            // 
-            this.label4.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label4.Location = new System.Drawing.Point(892, 4);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(589, 48);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "请选择想要进行的模式";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label4
             // 
             this.label4.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label4.Location = new System.Drawing.Point(892, 4);
+            this.label4.Location = new System.Drawing.Point(1189, 5);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(589, 48);
+            this.label4.Size = new System.Drawing.Size(785, 60);
             this.label4.TabIndex = 5;
             this.label4.Text = "请选择想要进行的模式";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -84,20 +76,19 @@
             // 
             this.label1.BackColor = System.Drawing.SystemColors.ControlLight;
             this.label1.Font = new System.Drawing.Font("宋体", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(399, 3);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(532, 4);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(491, 49);
+            this.label1.Size = new System.Drawing.Size(655, 61);
             this.label1.TabIndex = 4;
             this.label1.Text = "label1";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(2, 0);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Location = new System.Drawing.Point(3, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(128, 52);
+            this.button2.Size = new System.Drawing.Size(171, 65);
             this.button2.TabIndex = 3;
             this.button2.Text = "人人对战";
             this.button2.UseVisualStyleBackColor = true;
@@ -105,10 +96,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(267, 0);
-            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Location = new System.Drawing.Point(356, 0);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(128, 52);
+            this.button3.Size = new System.Drawing.Size(171, 65);
             this.button3.TabIndex = 2;
             this.button3.Text = "自动对战";
             this.button3.UseVisualStyleBackColor = true;
@@ -116,10 +107,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(134, 0);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(179, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 52);
+            this.button1.Size = new System.Drawing.Size(171, 65);
             this.button1.TabIndex = 0;
             this.button1.Text = "人机对战";
             this.button1.UseVisualStyleBackColor = true;
@@ -129,10 +120,10 @@
             // 
             this.panel3.Controls.Add(this.label2);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel3.Location = new System.Drawing.Point(0, 60);
-            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Location = new System.Drawing.Point(0, 75);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(743, 901);
+            this.panel3.Size = new System.Drawing.Size(991, 1126);
             this.panel3.TabIndex = 1;
             this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
             this.panel3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseMove);
@@ -142,10 +133,9 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Bisque;
             this.label2.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(317, 56);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(423, 70);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(63, 33);
+            this.label2.Size = new System.Drawing.Size(77, 40);
             this.label2.TabIndex = 0;
             this.label2.Text = "You";
             // 
@@ -153,10 +143,10 @@
             // 
             this.panel4.Controls.Add(this.label3);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel4.Location = new System.Drawing.Point(741, 60);
-            this.panel4.Margin = new System.Windows.Forms.Padding(2);
+            this.panel4.Location = new System.Drawing.Point(988, 75);
+            this.panel4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(743, 901);
+            this.panel4.Size = new System.Drawing.Size(991, 1126);
             this.panel4.TabIndex = 2;
             this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
             this.panel4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel4_MouseDown);
@@ -166,10 +156,9 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Bisque;
             this.label3.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(300, 56);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(400, 70);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(159, 33);
+            this.label3.Size = new System.Drawing.Size(197, 40);
             this.label3.TabIndex = 1;
             this.label3.Text = "Adversary";
             // 
@@ -180,18 +169,19 @@
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1484, 961);
+            this.panel1.Size = new System.Drawing.Size(1979, 1201);
             this.panel1.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1484, 961);
+            this.ClientSize = new System.Drawing.Size(1979, 1201);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
--- PlaneBombGame/Properties/Resources.Designer.cs
+++ PlaneBombGame/Properties/Resources.Designer.cs
@@ -106,6 +106,16 @@ namespace PlaneBombGame.Properties {
         }
         
         /// <summary>
+        ///   查找类似于 (图标) 的 System.Drawing.Icon 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Icon Plane {
+            get {
+                object obj = ResourceManager.GetObject("Plane", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
         internal static System.Drawing.Bitmap 击中 {
